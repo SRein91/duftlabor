9e3bd4bfb47c52561fc25306ebf979124a0ad6a9
Sonja Reinholz <sonjareinholz@Sonjas-Air.fritz.box>
2025-01-12 12:10:09 +0100

    neuen April Termin ergänzt

--- script.js
+++ script.js
@@ -24,22 +24,20 @@ function loadContent(page) {
 
                             <p>Unsere Workshops finden in regelmäßigen Abständen in Mannheim und Hamburg statt.</p>
 
-                            <h2>Nächste Termine</h2>
+                            <h2>Nächste freie Termine</h2>
                             <ul>
-                                <li>So, 16. Februar 2025 in Mannheim 
-                                <p>    10:00 Uhr - 13:30 Uhr (noch freie Plätze)</li>
-                                <li>Sa, 08. März 2025 in Hamburg 
-                                <p>    10:00 Uhr - 13:30 Uhr (ausgebucht)</li>
-                                <li>Sa, 08. März 2025 in Hamburg 
-                                <p>    14:30 Uhr - 18:00 Uhr (noch freie Plätze)</li>
-                                <li>Sa, 29. März 2025 in Mannheim 
-                                <p>    10:00 Uhr - 13:30 Uhr (noch freie Plätze)</li>
-                                <li>So, 30. März 2025 in Mannheim 
-                                <p>    10:00 Uhr - 13:30 Uhr (noch freie Plätze)</li>
-                                <li>Sa, 26. April 2025 in Mannheim 
-                                <p>    10:00 Uhr - 13:30 Uhr (noch freie Plätze)</li>
+                                <li>So, 16. Februar 2025 in Mannheim 10:00 Uhr - 13:30 Uhr</li>
+                                <li>Sa, 08. März 2025 in Hamburg 14:30 Uhr - 18:00 Uhr</li>
+                                <li>Sa, 29. März 2025 in Mannheim 10:00 Uhr - 13:30 Uhr</li>
+                                <li>So, 30. März 2025 in Mannheim 10:00 Uhr - 13:30 Uhr</li>
+                                <li>Sa, 12. April 2025 in Hamburg 10:00 Uhr - 14:30 Uhr</li>
+                                <li>Sa, 12. April 2025 in Hamburg 14:30 Uhr - 18:00 Uhr</li>
+                                <li>Sa, 26. April 2025 in Mannheim 10:00 Uhr - 13:30 Uhr</li>
                             </ul>
-
+                            <h2>Ausgebucht</h2>
+                            <ul>
+                            <li>Sa, 08. März 2025 in Hamburg 10:00 Uhr - 13:30 Uhr</li>
+                            </ul>
                             <h2>Wo</h2>
                             <p>Mannheim: Pâtisserie CØR<br>Lameystraße 17<br>68165 Mannheim</p>
                             <p>Hamburg: Alte Brotfabrik<br>Rellinger Str. 23<br>20257 Hamburg</p>
