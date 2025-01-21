6da3a8b5b64f4e516e05b0a6b6e34fcfbf3187e5
Sonja Reinholz <72928774+SRein91@users.noreply.github.com>
2025-01-21 09:45:29 +0100

    30.03 Termin ausgebucht

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function loadContent(page) {
                                 <ul>
                                     <li>So, 16.02.2025 10:00 Uhr - 13:30 Uhr (ausgebucht)</li>
                                     <li>Sa, 29.03.2025 10:00 Uhr - 13:30 Uhr</li>
-                                    <li>So, 30.03.2025 10:00 Uhr - 13:30 Uhr</li>
+                                    <li>So, 30.03.2025 10:00 Uhr - 13:30 Uhr (ausgebucht)</li>
                                     <li>Sa, 26.04.2025 10:00 Uhr - 13:30 Uhr</li>
                                 </ul>
                                 <h2>Wo</h2>
