0bf117c2c8abc6ffba25a277e6a4e6d5a4f90fc0
Sonja Reinholz <72928774+SRein91@users.noreply.github.com>
2025-09-02 10:01:17 +0200

    noch ein Platz 18.10

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function loadContent(page, push = true) {
                                 <ul>
                                     <li>Sa, 27.09.2025 10:00 Uhr - 13:00 Uhr (noch ein Platz)</li>
                                     <li>Sa, 27.09.2025 14:30 Uhr - 17:30 Uhr (ausgebucht)</li> 
-                                    <li>Sa, 18.10.2025 14:30 Uhr - 17:30 Uhr</li>
+                                    <li>Sa, 18.10.2025 14:30 Uhr - 17:30 Uhr (noch ein Platz)</li>
                                     <li>Do, 30.10.2025 17:30 Uhr - 20:30 Uhr</li>
                                     <li>Sa, 15.11.2025 14:30 Uhr - 17:30 Uhr</li>
                                     <li>Do, 20.11.2025 17:30 Uhr - 20:30 Uhr</li>
